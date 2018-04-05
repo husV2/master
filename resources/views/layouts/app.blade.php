bd45eb29f43a01a1f96afe706334735793d2eab6
Nooralogical <noora.etula@gmail.com>
2018-04-05 17:51:04 +0300

    Responsive menu and app.blade bootstraped, some fixes still needed (routing not done)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 	<script src="{{ URL::asset('js/Achievement.js') }}"></script>
 	<script src="{{ URL::asset('js/home.js') }}"></script>
 	<script src="{{ URL::asset('js/linkActivator.js') }}"></script>
+	<script src="{{ URL::asset('js/menu.js') }}"></script>
 	<script>
 	$(function () {
 		$.ajaxSetup({
@@ -25,84 +26,104 @@
 		});
 	});
 	</script>
-	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 		  integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="{{ URL::asset('css/home.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('css/menu.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/friend_request_message.css') }}">
 	<link rel="shortcut icon" href="{{{ asset('img/HUS_favicon.png') }}}">
     <link rel="stylesheet" href="{{ URL::asset('css/friend_request_message.css') }}">
 	<link rel="shortcut icon" href="{{ URL::asset('img/HUS_favicon.png') }}">
 	<link rel="notification icon" href="{{ URL::asset('img/apuri_notification.png') }}">
     @yield('header')
+
 </head>
 	<body>
 		@if(!empty(Auth::user()) && Auth::user()->friendRequests(true))
                     @include('partials.message')
 		@endif
-		<!-- Add message notification here-->
 
-		
+		<!-- Add message notification here
+
+		<div class="col-sm-2 col-sm-offset-10 col-logout">
+					<div class="link_placement">
+                                            @if(!Auth::guest())
+			<a class="link_user-profile" href="{{ url('/profile') }}" id="nav_profile">Tervetuloa, <?php echo  Auth::user()->username; ?>!</a>
+							<a class="link_logout" href="{{ url('/logout') }}">{{trans('main.logout')}}</a>
+                                            @endif
+				</div>
+            </div>
+
+-->
 		@if(!isset($nav_countdown))
 			@include('partials.training_notification')
 		@endif
-				
 		<div class="container main-container">
-			<div class="row">
-					<div class="col-md-1 col-md-push-11" style="text-align: right">
+			<header>
+				<a href="{{ url('/') }}" class="logo" data-scroll>{{trans('main.logo')}}</a>
+				<nav id="nav" role="navigation">
+					<a href="#nav" title="Show navigation">Show navigation</a>
+					<a href="#" title="Hide navigation">Hide navigation</a>
+					<ul>
+						<li class="menu-item"><a href="{{ url('/') }}" id="nav_treeni" data-scroll>{{ trans('main.home') }}</a></li>
+						<li class="menu-item"><a href="{{ url('/charts') }}" id="nav_charts" data-scroll>{{ trans('main.charts') }}</a></li>
+						<li class="menu-item"><a href="{{ url('/profile') }}" aria-haspopup="true" id="nav_profile" data-scroll>{{ trans('main.profile') }}</a>
+                            <ul>
+                                <li><a href='#'>Asetukset</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item"><a href="{{ url('/info') }}" aria-haspopup="true" id="nav_info" data-scroll>{{ trans('main.info') }}</a>
+							<ul>
+								<li><a href='#'>Työergonomia</a></li>
+								<li><a href='#'>Tietoturva</a></li>
+							</ul>
+						</li>
+                        @if(!Auth::guest() && Auth::user()->isAdmin)
+							<li class="menu-item"><a href="{{ url('/admin') }}" id="nav_admin" data-scroll>{{ trans('main.admin') }}</a></li>
+						@endif
+						<li class="menu-item"><a href="{{ url('/logout') }}" id="nav_logout" data-scroll>{{ trans('main.logout') }}</a></li>
+					</ul>
+				</nav>
+			</header>
+			<div class="row" id="nomargin">
+				<div class="col-sm-3 nopads">
+					<a href="{{ url('/') }}">
+					<img src="{{ URL::asset('img/logo.png') }}" class="img-responsive" id="logo-hus"></a>
+				</div>
+				<div class="col-sm-6">
+					@if(!isset($nav_countdown))
+						<div class="cd_cont"><p id="cd-title" style="display: none">{{trans('main.train_time')}}</p>
+							<button class="cd-accept" id="cd-ac-nav" style="display: none">{{trans('main.train_now')}}</button>
+							<a class="cd-snooze" id="cd-sn-nav" style="display: none">{{trans('main.train_later')}}</a>
+						</div>
+					@endif
+				</div>
+				<div class="col-sm-3">
+					<div class="col-md-12 nopads" id="timer-div">
 						@if(!isset($nav_countdown))
 							<p class="cd-clock" id="cd-nav">&nbsp;</p>
 						@else
 							<p>&nbsp;</p>
 						@endif
 					</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-3">
-					<a href="{{ url('/') }}">
-					<img src="{{ URL::asset('img/logo.png') }}" class="img-responsive"></a>
 				</div>
-				<div class="col-sm-9">
-					<div class="row" id="nomargin">
-						<div class="col-md-7 col-md-offset-1">
-							@if(!isset($nav_countdown))
-								<div class="cd_cont"><p id="cd-title" style="display: none">{{trans('main.train_time')}}</p>
-								<button class="cd-accept" id="cd-ac-nav" style="display: none">{{trans('main.train_now')}}</button>
-								<a class="cd-snooze" id="cd-sn-nav" style="display: none">{{trans('main.train_later')}}</a>
-								</div>
-							@endif
-						</div>
-						<div class="col-sm-2">
-							<div class="equal navbox">
-								<div class="text-right nopads"><a class="link_usernav" href="{{ url('/') }}" id="nav_treeni">{{ trans('main.home') }}</a></div>
-								<div class="text-right nopads"><a class="link_usernav" href="{{ url('/charts') }}" id="nav_charts">{{ trans('main.charts') }}</a></div>
-                                <div class="text-right nopads"><a class="link_usernav" href="{{ url('/profile') }}" id="nav_profile"><?php echo  Auth::user()->username; ?></a></div>
-								@if(!Auth::guest() && Auth::user()->isAdmin)
-									<div class="text-right nopads"><a class="link_usernav" href="{{ url('/admin') }}" id="nav_admin">{{ trans('main.admin') }}</a></div>
-								@endif
+			</div>
+			<div class="row" id="nomargin">
+				<div class="col-sm-2" id="avatar-div">
+						<div class="avatarbox_border" title="{{trans('main.profilepic_change')}}">
+							<div class="avatarbox_image">
+								<a href='#' onClick='show_profilepic_modal()'>
+									<img src="<?= URL::asset('storage/avatars/'.session('profilepic')) ?>" class="img-responsive center-block">
+								</a>
 							</div>
+							<div class="avatarbox_ribbon"></div>
 						</div>
-						<div class="col-sm-2 h_inherit">
-								<div class="avatarbox_border" title="{{trans('main.profilepic_change')}}">
-                                                                    <div class="avatarbox_image">
-                                                                        <a href='#' onClick='show_profilepic_modal()'>
-                                                                            <img src="<?= URL::asset('storage/avatars/'.session('profilepic')) ?>" class="img-responsive center-block">
-                                                                        </a>
-                                                                    </div>
-									<div class="avatarbox_ribbon"></div>
-								</div>
-						</div>
-					</div>
+                    <div class="col-sd-1">
+                        <a class="link_welcome-profile" href="{{ url('/profile') }}" id="nav_profile">{{trans('main.welcome-profile')}}, <?php echo  Auth::user()->firstname; ?>!</a>
+                    </div>
 				</div>
 			</div>
-            <div class="row">
-				<div class="col-sm-2 col-sm-offset-10 col-logout">
-					<div class="link_placement">
-                                            @if(!Auth::guest())
-						<a class="link_logout" href="{{ url('/logout') }}">{{trans('main.logout')}}</a>
-                                            @endif
-					</div>
-				</div>
+            <div class="row" id="nomargin">
 				@yield('content')
 				<div class="row">
 					<div class="col-sm-12">
@@ -119,5 +140,6 @@
                 @endif
                 @include('partials.achievement')
             @endif
+        <script src="js/menu.js"></script>
 	</body>
 </html>
